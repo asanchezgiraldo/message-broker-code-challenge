6a729d636f5a0db385ea76f4133b809d1df39d79
alejandro <alejandro.sanchezgiraldo@qantas.com.au>
2020-01-16 08:29:47 +1100

    fix README (#2)
    * fix README
    
    * updates

--- test/unit.sh
+++ test/unit.sh
@@ -1,13 +1,21 @@
 #!/bin/bash
-# Send Message and validate that this is showing in the logs
+# Feature: Send and log message
+
+# Scenarion: Send 10 messages and validate that they are showing in the logs
+
 counter=1
 while [ $counter -le 10 ]; do
+    # When I send a customer message
     MSG='message number: '$counter npm run send
+    
+    # Then I can see the message in the logs
     if grep -q 'message number: '$counter $(pwd)"/app/logs/combined.log"; then
-        echo "Message has bee revieved as per logs"
+        echo "Message has bee received as per logs"
     else
         exit 1
     fi
+
+    # And i can move to the next iteration
     ((counter++))
 done
 
